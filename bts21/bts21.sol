b8ec2c235be3a6e59b8b9971d7e3163bffe11263
masayoshikob <masayoshikob@gmail.com>
2023-08-26 18:05:15 +1200

    embed security features

--- bts21/bts21.sol
+++ bts21/bts21.sol
@@ -3,11 +3,7 @@
 
 pragma solidity ^0.8.18;
 
-// Import Ownable and ReentrancyGuard from the specified repositories
-import "https://raw.githubusercontent.com/BitnetMoney/contract-standards/main/library/Ownable.sol";
-import "https://raw.githubusercontent.com/BitnetMoney/contract-standards/main/library/ReentrancyGuard.sol";
-
-contract BTS21 is Ownable, ReentrancyGuard {
+contract BTS21 {
     string private _tokenName;
     string private _tokenSymbol;
     uint8 private _tokenDecimals;
@@ -20,11 +16,27 @@ contract BTS21 is Ownable, ReentrancyGuard {
 
     uint256 public oraclePrice; // Oracle price variable
 
+    address private _owner;
+
+    bool private _notEntered;
+
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
     event FrozenAccount(address indexed account, bool isFrozen); // Event for frozen account state change
     event OracleAdded(address indexed oracle); // Event for oracle addition
     event OracleRemoved(address indexed oracle); // Event for oracle removal
+
+    modifier onlyOwner() {
+        require(isOwner(), "BTS21: caller is not the owner");
+        _;
+    }
+
+    modifier nonReentrant() {
+        require(_notEntered, "BTS21: reentrant call");
+        _notEntered = false;
+        _;
+        _notEntered = true;
+    }
 
     constructor(string memory name, string memory symbol, uint8 decimals, uint256 initialSupply) {
         _tokenName = name;
@@ -32,31 +44,31 @@ contract BTS21 is Ownable, ReentrancyGuard {
         _tokenDecimals = decimals;
         _tokenTotalSupply = initialSupply * 10 ** uint256(decimals);
         _balances[msg.sender] = _tokenTotalSupply;
+        _owner = msg.sender;
+        _notEntered = true;
     }
 
-    // Getter functions for token details
-    function getTokenName() external view returns (string memory) {
-        return _tokenName;
+    function owner() public view returns (address) {
+        return _owner;
     }
 
-    function getTokenSymbol() external view returns (string memory) {
-        return _tokenSymbol;
+    function isOwner() public view returns (bool) {
+        return msg.sender == _owner;
     }
 
-    function getTokenDecimals() external view returns (uint8) {
-        return _tokenDecimals;
+    function renounceOwnership() public onlyOwner {
+        _owner = address(0);
     }
 
-    function getTokenTotalSupply() external view returns (uint256) {
-        return _tokenTotalSupply;
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "BTS21: new owner is the zero address");
+        _owner = newOwner;
     }
 
-    // Balance of an account
     function balanceOf(address account) external view returns (uint256) {
         return _balances[account];
     }
 
-    // Transfer function with reentrancy protection
     function transfer(address recipient, uint256 amount) external nonReentrant returns (bool) {
         require(!_frozenAccounts[msg.sender], "BTS21: sender account is frozen");
         require(!_frozenAccounts[recipient], "BTS21: recipient account is frozen");
@@ -70,14 +82,12 @@ contract BTS21 is Ownable, ReentrancyGuard {
         return true;
     }
 
-    // Approve allowance for spender
     function approve(address spender, uint256 amount) external returns (bool) {
         _allowances[msg.sender][spender] = amount;
         emit Approval(msg.sender, spender, amount);
         return true;
     }
 
-    // Transfer from approved allowance with reentrancy protection
     function transferFrom(address sender, address recipient, uint256 amount) external nonReentrant returns (bool) {
         require(!_frozenAccounts[sender], "BTS21: sender account is frozen");
         require(!_frozenAccounts[recipient], "BTS21: recipient account is frozen");
@@ -94,14 +104,12 @@ contract BTS21 is Ownable, ReentrancyGuard {
         return true;
     }
 
-    // Increase allowance for spender
     function increaseAllowance(address spender, uint256 addedValue) external returns (bool) {
         _allowances[msg.sender][spender] += addedValue;
         emit Approval(msg.sender, spender, _allowances[msg.sender][spender]);
         return true;
     }
 
-    // Decrease allowance for spender
     function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool) {
         uint256 currentAllowance = _allowances[msg.sender][spender];
         require(currentAllowance >= subtractedValue, "BTS21: decreased allowance below zero");
@@ -111,36 +119,30 @@ contract BTS21 is Ownable, ReentrancyGuard {
         return true;
     }
 
-    // Function to freeze/unfreeze an account, accessible only by the owner
     function freezeAccount(address account, bool isFrozen) external onlyOwner {
         _frozenAccounts[account] = isFrozen;
         emit FrozenAccount(account, isFrozen);
     }
 
-    // Function to set the oracle price of the token, accessible only by oracles
     function setOraclePrice(uint256 price) external onlyOracle {
         oraclePrice = price;
     }
 
-    // Function to add an oracle, accessible only by the owner
     function addOracle(address oracle) external onlyOwner {
         require(oracle != address(0), "BTS21: oracle address cannot be zero");
         _oracles[oracle] = true;
         emit OracleAdded(oracle);
     }
 
-    // Function to remove an oracle, accessible only by the owner
     function removeOracle(address oracle) external onlyOwner {
         _oracles[oracle] = false;
         emit OracleRemoved(oracle);
     }
 
-    // Function to check if an address is an oracle
     function isOracle(address account) external view returns (bool) {
         return _oracles[account];
     }
 
-    // Modifier to check if the caller is an oracle
     modifier onlyOracle() {
         require(_oracles[msg.sender], "BTS21: caller is not an oracle");
         _;
