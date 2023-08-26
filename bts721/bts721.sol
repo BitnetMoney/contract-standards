f5cfad81d3dcfd63fccd6536af1ea8b288aea08f
masayoshikob <masayoshikob@gmail.com>
2023-08-27 06:42:27 +1200

    fix bts721

--- bts721/bts721.sol
+++ bts721/bts721.sol
@@ -43,103 +43,66 @@ contract Ownable {
     }
 }
 
-// BTS20 Token Contract
-contract BTS20 is Ownable {
-    // Private variables to store token information
-    string private _tokenName;
-    string private _tokenSymbol;
-    uint8 private _tokenDecimals;
-    uint256 private _tokenTotalSupply;
+// Bitnet Token Standard 721 (BTS721) implementation
+contract BTS721 is Ownable {
+    // Mapping to track token ownership
+    mapping(uint256 => address) private _owners;
 
-    // Mapping to track balances of token holders
+    // Mapping to track token balances of owners
     mapping(address => uint256) private _balances;
-    
-    // Mapping to track approved allowances for spending tokens
-    mapping(address => mapping(address => uint256)) private _allowances;
 
-    // Events to emit when transfers or approvals occur
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    // Event emitted when token ownership changes
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
 
-    // Constructor to initialize token details and assign initial supply
-    constructor(string memory name, string memory symbol, uint8 decimals, uint256 initialSupply) {
-        _tokenName = name;
-        _tokenSymbol = symbol;
-        _tokenDecimals = decimals;
-        _tokenTotalSupply = initialSupply * 10 ** uint256(decimals);
-        _balances[msg.sender] = _tokenTotalSupply;
+    // Constructor to initialize token name and symbol
+    constructor(string memory _name, string memory _symbol) {
+        name = _name;
+        symbol = _symbol;
     }
 
-    // Getter functions for token information
-    function getTokenName() external view returns (string memory) {
-        return _tokenName;
+    string private name;
+    string private symbol;
+
+    // Function to get the name of the token
+    function getName() external view returns (string memory) {
+        return name;
     }
 
-    function getTokenSymbol() external view returns (string memory) {
-        return _tokenSymbol;
+    // Function to get the symbol of the token
+    function getSymbol() external view returns (string memory) {
+        return symbol;
     }
 
-    function getTokenDecimals() external view returns (uint8) {
-        return _tokenDecimals;
+    // Function to get the owner of a specific token
+    function ownerOf(uint256 tokenId) external view returns (address) {
+        return _owners[tokenId];
     }
 
-    function getTokenTotalSupply() external view returns (uint256) {
-        return _tokenTotalSupply;
+    // Function to get the balance of tokens owned by an address
+    function balanceOf(address owner) external view returns (uint256) {
+        return _balances[owner];
     }
 
-    // Function to retrieve balance of a token holder
-    function balanceOf(address account) external view returns (uint256) {
-        return _balances[account];
+    // Function to mint a new token and assign ownership
+    function mint(address to, uint256 tokenId) external {
+        require(to != address(0), "BTS721: mint to the zero address");
+        require(_owners[tokenId] == address(0), "BTS721: token already minted");
+
+        _balances[to] += 1;
+        _owners[tokenId] = to;
+
+        emit Transfer(address(0), to, tokenId);
     }
 
-    // Function to transfer tokens to another address
-    function transfer(address recipient, uint256 amount) external returns (bool) {
-        require(recipient != address(0), "BTS20: transfer to the zero address");
-        require(_balances[msg.sender] >= amount, "BTS20: insufficient balance");
+    string private _baseTokenURI;
 
-        _balances[msg.sender] -= amount;
-        _balances[recipient] += amount;
-
-        emit Transfer(msg.sender, recipient, amount);
-        return true;
+    // Function to return the base URI for token metadata
+    function baseTokenURI() external view returns (string memory) {
+        return _baseTokenURI;
     }
 
-    // Function to approve a spender to spend a certain amount of tokens on behalf of the caller
-    function approve(address spender, uint256 amount) external returns (bool) {
-        _allowances[msg.sender][spender] = amount;
-        emit Approval(msg.sender, spender, amount);
-        return true;
-    }
-
-    // Function to transfer tokens from one address to another with allowance
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool) {
-        require(sender != address(0), "BTS20: transfer from the zero address");
-        require(recipient != address(0), "BTS20: transfer to the zero address");
-        require(_balances[sender] >= amount, "BTS20: insufficient balance");
-        require(_allowances[sender][msg.sender] >= amount, "BTS20: allowance exceeded");
-
-        _balances[sender] -= amount;
-        _balances[recipient] += amount;
-        _allowances[sender][msg.sender] -= amount;
-
-        emit Transfer(sender, recipient, amount);
-        return true;
-    }
-
-    // Function to increase the allowance for a spender
-    function increaseAllowance(address spender, uint256 addedValue) external returns (bool) {
-        _allowances[msg.sender][spender] += addedValue;
-        emit Approval(msg.sender, spender, _allowances[msg.sender][spender]);
-        return true;
-    }
-
-    // Function to decrease the allowance for a spender
-    function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool) {
-        uint256 currentAllowance = _allowances[msg.sender][spender];
-        require(currentAllowance >= subtractedValue, "BTS20: decreased allowance below zero");
-
-        _allowances[msg.sender][spender] = currentAllowance - subtractedValue;
-        emit Approval(msg.sender, spender, _allowances[msg.sender][spender]);
-        return true;
+    // Function to set a new base URI for token metadata
+    function setBaseTokenURI(string memory newBaseURI) external onlyOwner {
+        _baseTokenURI = newBaseURI;
     }
 }
